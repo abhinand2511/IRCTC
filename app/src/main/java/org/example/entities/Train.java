7882bbda255d851de71d269d0c00abcb4043d68f
abhinand2511 <abhinand25112001@gmail.com>
2025-09-04 23:09:36 -0700

    Train file and services folder

--- app/src/main/java/org/example/entities/Train.java
+++ app/src/main/java/org/example/entities/Train.java
@@ -1,4 +1,11 @@
 package org.example.entities;
 
+import java.util.*;
+
 public class Train {
+    private String trainId;
+    private String trainNo;
+    private List<List<Integer>> seats;
+    private Map<String, Time> stationTimes;
+    private List<String> stations;
 }
