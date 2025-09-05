9a6a5516b541c94731f12d3e72c11f02386f3b5a
abhinand2511 <abhinand25112001@gmail.com>
2025-09-05 01:22:51 -0700

    jackson dependencies

---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,21 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.User;
+
+import java.io.File;
+import java.util.List;
+
+public class UserBookingService {
+    private User user;
+    private List<User> userList;
+    private ObjectMapper objectMapper  = new ObjectMapper()
+    private static final String USERS_PATH = "../localDb/users.json"
+    public UserBookingService(User user){
+        this.user = user;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        })
+    }
+}
